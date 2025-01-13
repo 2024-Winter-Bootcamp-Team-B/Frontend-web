809606fc0488ad8188bb11b032e63b640887e7ef
성태경 <taegyeong0320@naver.com>
2025-01-14 04:10:07 +0900

    design: BlockedURLPage 퍼블리싱 수정

--- src/pages/BlockedURLPage.tsx
+++ src/pages/BlockedURLPage.tsx
@@ -1,21 +1,21 @@
 const BlockedURLPage = () => {
   return (
-    <div className='section overflow-y-hidden'>
-      <div className='flex flex-col justify-center items-center min-h-screen gap-20'>
-        <div className='w-[1100px] h-[300px] flex flex-col justify-center'>
-          <div className='font-abril text-8xl m-2 p-4'>
-            This page is blocked
-          </div>
-          <div className='font-paperlogy font-normal text-2xl m-2 p-4'>
-            이 사이트는 차단되었습니다.<br></br>하단의 뒤로가기 버튼을 누르세요.
-          </div>
+    <div className='w-full flex flex-col items-center py-8'>
+      <div className='flex flex-col gap-8 py-8 h-full'>
+        <p className='font-abril text-8xl'>This site is blocked</p>
+        <div className='font-light text-2xl'>
+          <p>이 사이트는 차단되었습니다.</p>
+          <p>하단의 뒤로가기 버튼을 누르세요.</p>
         </div>
-
-        <div className=' w-[1100px] h-[100px] flex flex-col items-center justify-center'>
-          <button className='font-paperlogy font-normal text-2xl px-4 py-4 bg-white text-focus-color rounded-lg'>
-            뒤로가기
-          </button>
-        </div>
+        <button
+          className='bg-white rounded-3xl w-24 h-12 self-center mt-auto'
+          style={{
+            boxShadow:
+              '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
+          }}
+        >
+          뒤로가기
+        </button>
       </div>
     </div>
   );
