26372d4812006ff9dc1cd041b8d0ef357697ed95
sua-l3e <sualee1994@gmail.com>
2025-01-14 01:45:46 +0900

    design: 차단된 사이트 디자인

--- src/pages/BlockedURLPage.tsx
+++ src/pages/BlockedURLPage.tsx
@@ -1,5 +1,24 @@
 const BlockedURLPage = () => {
-  return <div className='section h-full'></div>;
+  return (
+    <div className='section overflow-y-hidden'>
+      <div className='flex flex-col justify-center items-center min-h-screen gap-20'>
+        <div className='w-[1100px] h-[300px] flex flex-col justify-center'>
+          <div className='font-abril text-8xl m-2 p-4'>
+            This page is blocked
+          </div>
+          <div className='font-paperlogy font-normal text-2xl m-2 p-4'>
+            이 사이트는 차단되었습니다.<br></br>하단의 뒤로가기 버튼을 누르세요.
+          </div>
+        </div>
+
+        <div className=' w-[1100px] h-[100px] flex flex-col items-center justify-center'>
+          <button className='font-paperlogy font-normal text-2xl px-4 py-4 bg-white text-focus-color rounded-lg'>
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlockedURLPage;
