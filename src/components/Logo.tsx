428134b00334f11db5cd032a79443e60ae8ab69c
성태경 <taegyeong0320@naver.com>
2025-01-14 22:08:24 +0900

    design: 밈 페이지 퍼블리싱

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className='flex min-h-11 max-h-11 mx-16 w-full'>
-      <p className='font-cinzel font-bold text-[#242320] ml-auto'>FOCUS</p>
+      <p className='font-cinzel font-bold text-focus-color ml-auto'>FOCUS</p>
     </div>
   );
 };
--- src/pages/MemePage.tsx
+++ src/pages/MemePage.tsx
@@ -1,9 +1,28 @@
 import Logo from '../components/Logo';
+import jail from '../assets/jail.svg';
+import meme from '../assets/memeEx.svg';
 
 const MemePage = () => {
   return (
     <div className='w-full flex flex-col items-center px-16 py-8'>
       <Logo />
+      <div className='flex gap-8'>
+        <div className='flex flex-col grow w-full justify-between'>
+          <div className='text-white flex gap-4'>
+            <p className='font-cinzel font-bold text-2xl'>FOCUS</p>
+            <div className='bg-white w-full h-px self-center' />
+          </div>
+          <div className='font-semibold text-4xl'>
+            <p>전체 목표의 62.5%를 달성했습니다</p>
+            <p>목표시간 4H 달성시간 2H 30M</p>
+          </div>
+          <div className='bg-white h-12 rounded-3xl' />
+          <img src={jail} />
+        </div>
+        <div className='grow w-full'>
+          <img src={meme} />
+        </div>
+      </div>
     </div>
   );
 };
