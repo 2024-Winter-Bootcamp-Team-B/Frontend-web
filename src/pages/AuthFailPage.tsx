f89ed464adca085f07f6ddee6d2134e326577467
성태경 <taegyeong0320@naver.com>
2025-01-15 00:07:28 +0900

    design: 인증 실패 페이지 수정

--- src/pages/AuthFailPage.tsx
+++ src/pages/AuthFailPage.tsx
@@ -1,33 +1,21 @@
-import prisoner from '../assets/image707.svg';
-import prison from '../assets/Union.png';
+import authFail from '../assets/authFail.svg';
 
 const AuthFailPage = () => {
   return (
-    <div className='flex flex-col relative h-full w-full justify-center items-center gap-8'>
-      <div className='flex flex-col items-center w-full h-3/5 relative bottom-[90px]'>
-        <div className=' relative w-full h-full z-10'>
-          <img className='w-full h-full z-10' src={prison}></img>
-        </div>
-        <div className=' h-[1200px] w-[1521px] z-0 relative bottom-[1300px]'>
-          <img className='w-full h-full' src={prisoner}></img>
-        </div>
-      </div>
-      <div className=' relative font-abril text-[300px]'>Try Again</div>
-      <div className='relative'>
-        <button
-          className='bg-white rounded-3xl w-[500px] h-[250px] self-center mt-auto'
-          style={{
-            boxShadow:
-              '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
-          }}
-        >
-          <p className='font-paperlogy text-[100px]'>재인증</p>
-        </button>
-      </div>
+    <div className='w-full flex flex-col items-center justify-between py-8'>
+      <img src={authFail} className='h-4/5 w-full' />
+      <p className='font-abril text-5xl'>Try Again</p>
+      <button
+        className='bg-white rounded-3xl w-24 h-12'
+        style={{
+          boxShadow:
+            '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
+        }}
+      >
+        재인증
+      </button>
     </div>
   );
 };
 
 export default AuthFailPage;
-
-// slkdjfigjeirhg
