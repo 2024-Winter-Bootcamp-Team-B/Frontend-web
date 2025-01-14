0c4c92ea22e2ded3d9af772b87290d4abb4be4fc
성태경 <taegyeong0320@naver.com>
2025-01-14 17:37:35 +0900

    design: 차단 해제 페이지 퍼블리싱

--- src/pages/UnblockPage.tsx
+++ src/pages/UnblockPage.tsx
@@ -1,7 +1,23 @@
+import Navbar from '../components/Navbar';
+import jail from '../assets/jail.svg';
+
 const UnblockPage = () => {
   return (
-    <div className='section'>
-      <div>UnblockPage</div>
+    <div className='section h-full'>
+      <Navbar />
+      <div className='flex flex-col items-center h-[calc(100%-2.75rem)] px-16 py-8 gap-6'>
+        <div className='bg-white w-[800px] h-12 rounded-3xl' />
+        <button
+          className='bg-white rounded-3xl w-24 h-12'
+          style={{
+            boxShadow:
+              '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
+          }}
+        >
+          차단 해제
+        </button>
+        <img src={jail} />
+      </div>
     </div>
   );
 };
