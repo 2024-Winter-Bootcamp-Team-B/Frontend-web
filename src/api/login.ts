faac06d8858a685b20420e1341659b2638b703c6
성태경 <taegyeong0320@naver.com>
2025-01-18 09:14:43 +0900

    refactor: 로그인 API 코드 변경

--- src/api/login.ts
+++ src/api/login.ts
@@ -9,20 +9,23 @@ export interface LoginRes {
   message: string;
   user_id?: string;
   user_login_id?: string;
-  user_login_password?: string;
   user_name?: string;
   user_email?: string;
 }
 
 export const fetchLogin = async (req: LoginReq) => {
+  const { login_id, login_password } = req;
   try {
-    const { login_id, login_password } = req;
-    const response = await api.post<LoginRes>(`/user/login-g`, {
-      login_id,
-      login_password,
-    });
+    const response = await api.post<LoginRes>(
+      '/user/login-g',
+      {
+        login_id,
+        login_password,
+      },
+      { withCredentials: true },
+    );
     return response.data;
   } catch (error) {
-    console.error(error);
+    Promise.reject(error);
   }
 };
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,32 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { LoginReq, fetchLogin } from '../api/login';
 
 const LoginPage = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const login: LoginReq = { login_id: id, login_password: password };
-    try {
-      const response = await fetchLogin(login);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+    fetchLogin(login)
+      .then((response) => {
+        if (response) {
+          console.log(response);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor='id'>아이디</label>
-        <input id='id' value={id} onChange={(e) => setId(e.target.value)} />
+        <input id='id' value={id} onChange={handleIdChange} />
         <label htmlFor='password'>비밀번호</label>
         <input
           id='password'
           type='password'
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handlePasswordChange}
         />
         <button>로그인</button>
       </form>
