17bcd453f24e28d2fe958d5dea4c4b2add1ce4c7
성태경 <taegyeong0320@naver.com>
2025-01-22 01:43:49 +0900

    feat: 사진 인증 페이지 기능 구현

--- src/pages/PhotoAuthPage.tsx
+++ src/pages/PhotoAuthPage.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Logo from '../components/Logo';
 import { fetchUploadImg, UploadImgReq } from '../api/uploadImage';
+import useAuthStore from '../store/authStore';
+import photoButton from '../assets/photoButton.svg';
 
 const PhotoAuthPage = () => {
   const [file, setFile] = useState<File | null>(null);
+  const { user_id } = useAuthStore();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -12,16 +15,18 @@ const PhotoAuthPage = () => {
     }
   };
 
-  // 연동 확인 안됨
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (!file) {
+      return;
+    }
+    if (!user_id) {
       return;
     }
     const uploadImage: UploadImgReq = {
       hand_shape: [1, 1, 1, 1, 1],
       file: file,
-      user_id: 1,
+      user_id,
     };
     fetchUploadImg(uploadImage)
       .then((response) => {
@@ -38,11 +43,20 @@ const PhotoAuthPage = () => {
       <p>예시 사진대로 사진을 찍어 업로드하세요</p>
       <div className='flex gap-3'>
         <div className='w-[300px] h-[400px] bg-[#D1D5DB] rounded-md'></div>
-        <form onSubmit={handleSubmit}>
+        <div className='w-[300px] h-[400px] bg-[#D1D5DB] rounded-md'>
           <label
             htmlFor='image'
-            className='flex w-[300px] h-[400px] bg-[#D1D5DB] rounded-md cursor-pointer'
-          ></label>
+            className='h-full flex justify-center items-center cursor-pointer'
+          >
+            {file ? (
+              <img
+                src={URL.createObjectURL(file)}
+                className='w-full h-full object-cover rounded-md'
+              />
+            ) : (
+              <img src={photoButton} className='w-16 h-16' />
+            )}
+          </label>
           <input
             id='image'
             type='file'
@@ -50,16 +64,18 @@ const PhotoAuthPage = () => {
             className='hidden'
             onChange={handleFileChange}
           />
-          <input
-            type='submit'
-            className='bg-white rounded-3xl w-24 h-12 self-center mt-auto'
-            style={{
-              boxShadow:
-                '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
-            }}
-          />
-        </form>
+        </div>
       </div>
+      <button
+        onClick={handleSubmit}
+        className='bg-white rounded-3xl w-24 h-12 self-center'
+        style={{
+          boxShadow:
+            '0px 2px 8px 0px rgba(40, 41, 61, 0.08), 0px 20px 32px 0px rgba(96, 97, 112, 0.24)',
+        }}
+      >
+        제출
+      </button>
     </div>
   );
 };
