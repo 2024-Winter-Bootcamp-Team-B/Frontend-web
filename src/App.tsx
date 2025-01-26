7f8990838b3f024f1f0991afead2ca5850f92e8e
sua-l3e <sualee1994@gmail.com>
2025-01-26 20:29:01 +0900

    feat: Navbar 고정 최종완료, 페이지별 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import FullPage from './pages/FullPage';
 import Navbar from './components/Navbar'; // Navbar 추가
 import BlockedURLPage from './pages/BlockedURLPage';
@@ -10,10 +10,23 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 
 function App() {
+  const location = useLocation();
+
+  // Navbar를 표시할 경로를 설정합니다.
+  const showNavbarPaths = [
+    '/',
+    '/main',
+    '/explain',
+    '/block',
+    '/unblock',
+    '/stats',
+  ];
+  const showNavbar = showNavbarPaths.includes(location.pathname);
+
   return (
     <>
       {/* Navbar를 최상단에 추가 */}
-      <Navbar />
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path='/' element={<FullPage />} />
         <Route path='/blockedURL' element={<BlockedURLPage />} />
