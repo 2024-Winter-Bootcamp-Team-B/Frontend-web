9dd9e6ed7153c48f4d6ad7e0fbebd3df0f5c4d00
성태경 <taegyeong0320@naver.com>
2025-01-29 02:29:54 +0900

    design: 메인, 설명 페이지 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 const MainPage = () => {
   return (
     <div className='section h-full'>
-      <div className='flex flex-col gap-7 justify-center w-2/5 h-[calc(100%-2.75rem)] mt-11'>
+      <div className='flex flex-col gap-7 justify-center absolute h-[calc(100%-2.75rem)] ml-44 mt-11'>
         <p
           className='font-cinzel font-bold text-8xl self-center text-focus-color'
           style={{
@@ -10,7 +10,7 @@ const MainPage = () => {
         >
           FOCUS
         </p>
-        <div className='flex flex-col items-end text-2xl mr-[10%]'>
+        <div className='flex flex-col items-end text-2xl'>
           <p className='mr-[15%]'>Filter Out Chaos,</p>
           <p>Unlock Success!</p>
         </div>
